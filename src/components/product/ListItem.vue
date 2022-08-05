97eef28a4a8a8e091b503fecc3c8452c754cacf4
ashkanfekri <ashkanfekridev@gmail.com>
2022-08-05 11:59:11 +0430

    Update ListItem.vue

--- src/components/product/ListItem.vue
+++ src/components/product/ListItem.vue
@@ -3,19 +3,48 @@
     <td>{{ product.id }}</td>
     <td>{{ product.name }}</td>
     <td>{{ product.price }}</td>
+    <td>
+      <button class="btn-delete" @click="deleteProduct">delete</button>
+    </td>
   </tr>
 </template>
 
 <script>
+import {APIs, HTTP} from "../../plugins/http.js";
+import store from "../../plugins/store.js";
+
 export default {
   name: "ListItem",
-  props: ['product']
+  props: ['product'],
+  methods: {
+    deleteProduct() {
+      let api = APIs.product.delete
+      console.log(this.product.id)
+      api.url = api.url.replace('{id}', this.product.id)
+      HTTP(api)
+          .then(response => {
+            console.log(response)
+            this.$swal('حذف محصول شما با موفقیت انجام شد.');
+          })
+          .catch(e => {
+            console.log(e)
+          })
+    }
+  }
+
 }
 </script>
 
 <style scoped>
-td{
+td {
   padding: 5px 15px;
   border: 1px solid #ffffff;
 }
+
+.btn-delete {
+  border: 1px solid #f61111;
+  color: #ffffff;
+  background: #f61111;
+}
+
 </style>
