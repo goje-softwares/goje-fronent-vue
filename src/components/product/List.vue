47184ff2224da0443798ed04887167a731527b1a
ashkanfekri <ashkanfekridev@gmail.com>
2022-08-05 11:03:00 +0430

    Create List.vue

---
+++ src/components/product/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <table>
+      <slot></slot>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List",
+}
+
+</script>
+
+<style scoped>
+table, tr{
+  border-collapse: collapse;
+  border: 1px solid #ffffff;
+}
+</style>
