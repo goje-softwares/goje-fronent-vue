3956b43bc57a7a86ad6b20e92cef5c6df8887ce1
ashkanfekri <ashkanfekridev@gmail.com>
2022-08-05 11:59:16 +0430

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import './style.css'
 import App from './App.vue'
 import './plugins/http.js'
 import Router from "./plugins/router.js";
+import VueSweetalert2 from 'vue-sweetalert2';
+
+import 'sweetalert2/dist/sweetalert2.min.css';
+
 
 
 createApp(App)
     .use(Router)
+    .use(VueSweetalert2)
     .mount('#app')
