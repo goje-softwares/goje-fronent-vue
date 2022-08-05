9fd62fd591ba339fc038116add4686a82abd978e
ashkanfekri <ashkanfekridev@gmail.com>
2022-08-05 11:02:54 +0430

    create form components

---
+++ src/components/form/submit-button.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <button>submit</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "submit-button"
+}
+</script>
+
+<style scoped>
+button{
+  margin-top: 25px;
+}
+</style>
---
+++ src/components/form/text-input.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default {
+  name: "text-input",
+
+  props: ['inputLabel', 'modelValue'],
+  emits: ['update:modelValue']
+}
+</script>
+
+<template>
+  <label>{{ inputLabel }}</label>
+  <input
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+
+<style scoped>
+label, input {
+  display: block;
+  color: #f9f9f9;
+  padding: 8px 15px;
+}
+</style>
