21456aaf5057c4be072cf05ace59630737c3babf
ashkanfekri <ashkanfekridev@gmail.com>
2022-08-05 11:00:56 +0430

    update for add router view and nav

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <header>
+    <ul class="nav">
+      <li>
+        <router-link :to="{name:'home'}">home</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'login'}">login</router-link>
+      </li>
+      <li>
+        <router-link :to="{name:'register'}">register</router-link>
+      </li>
+    </ul>
+  </header>
+
+  <router-view></router-view>
 </template>
 
 <style scoped>
@@ -22,10 +26,22 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
+
+ul.nav{
+  list-style: none;
+  display: flex;
+}
+ul.nav a{
+  padding: 8px;
+  display: block;
+}
+
 </style>
