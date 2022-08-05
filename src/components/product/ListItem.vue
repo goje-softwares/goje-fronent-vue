267fe6bbef98945a33a6bc029c3b4a9638324214
ashkanfekri <ashkanfekridev@gmail.com>
2022-08-05 11:03:06 +0430

    Create ListItem.vue

---
+++ src/components/product/ListItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <tr>
+    <td>{{ product.id }}</td>
+    <td>{{ product.name }}</td>
+    <td>{{ product.price }}</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "ListItem",
+  props: ['product']
+}
+</script>
+
+<style scoped>
+td{
+  padding: 5px 15px;
+  border: 1px solid #ffffff;
+}
+</style>
