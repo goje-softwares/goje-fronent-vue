94226655a3690793048e48635ca991a67ac048c1
ashkanfekri <ashkanfekridev@gmail.com>
2022-08-05 11:03:37 +0430

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import './plugins/http.js'
+import Router from "./plugins/router.js";
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .use(Router)
+    .mount('#app')
