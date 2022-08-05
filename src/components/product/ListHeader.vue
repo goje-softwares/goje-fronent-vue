38e16307cf08d4903edbaf35dae46f8c0a612ed9
ashkanfekri <ashkanfekridev@gmail.com>
2022-08-05 11:03:03 +0430

    Create ListHeader.vue

---
+++ src/components/product/ListHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <tr>
+    <td>id</td>
+    <td>name</td>
+    <td>price</td>
+  </tr>
+</template>
+
+<script>
+export default {
+  name: "ListHeader",
+}
+</script>
+
+<style scoped>
+
+td{
+  padding: 5px 15px;
+  border: 1px solid #ffffff;
+}
+</style>
