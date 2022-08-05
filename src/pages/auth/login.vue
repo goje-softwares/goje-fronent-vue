082b33f693fc5e64afb92fdd59a60ce422ea25b3
ashkanfekri <ashkanfekridev@gmail.com>
2022-08-05 11:03:10 +0430

    Create login.vue

---
+++ src/pages/auth/login.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div class="form-group">
+      <text-input
+          v-model="email"
+          input-label="email"
+      />
+      <text-input
+          v-model="password"
+          input-label="password"
+      />
+      <submit-button @click="login"></submit-button>
+    </div>
+  </div>
+</template>
+
+<script>
+import SubmitButton from "./../../components/form/submit-button.vue";
+import TextInput from "./../../components/form/text-input.vue";
+import {HTTP} from "../../plugins/http.js";
+import store from "../../plugins/store.js";
+
+export default {
+  name: "login",
+  components: {SubmitButton, TextInput},
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  }, methods: {
+    login() {
+      let data = {
+        'email': this.$data.email,
+        'password': this.$data.password
+      };
+
+      HTTP.postForm(`api/auth/login`, data)
+          .then(response => {
+            if (response.status === 200) {
+              store.setVal('auth.token', response.data.access_token)
+              store.setVal('userLogged', true)
+            }
+          })
+          .catch(e => {
+            console.log(e)
+          })
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
