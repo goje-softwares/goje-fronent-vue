9cc5033bf398cadfced629a5e5aeb587fbcd23d1
ashkanfekri <ashkanfekridev@gmail.com>
2022-08-10 05:53:26 +0430

    Create Empty.vue

---
+++ src/layouts/Empty.vue
@@ -0,0 +1,17 @@
+<template>
+<main id="main">
+<slot></slot>
+</main>
+</template>
+
+<script>
+import "./../assets/css/base.css"
+
+export default {
+  name: "Empty"
+}
+</script>
+
+<style scoped>
+
+</style>
