2f3b80b8a5e12dc2157426075b6be3929999ac63
ashkanfekri <ashkanfekridev@gmail.com>
2022-08-10 05:53:57 +0430

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,68 @@
-<script setup>
+<script>
 
+import Dashboard from "./layouts/Dashboard.vue";
+import Empty from "./layouts/Empty.vue";
+
+export default {
+  name: 'App',
+  components: {Empty, Dashboard},
+  data() {
+    return {
+      userLogged: false,
+      navlinks: {
+        home: {
+          name: 'home',
+          title: 'داشبورد',
+          icon: 'book'
+        },
+        productCreate: {
+          name: 'product.create',
+          title: 'ایجاد محصول',
+          icon: 'book'
+        },
+        login: {
+          name: 'login',
+          title: 'login',
+          icon: 'book'
+        },
+        register: {
+          name: 'register',
+          title: 'register',
+          icon: 'book'
+        }
+      }
+    }
+  }, mounted() {
+    this.userLogged = !!(localStorage.getItem('auth.token'));
+  }
+}
 </script>
 
 <template>
-  <header>
-    <ul class="nav">
-      <li>
-        <router-link :to="{name:'home'}">home</router-link>
-      </li>
-      <li>
-        <router-link :to="{name:'product.create'}">create product</router-link>
-      </li>
-      <li>
-        <router-link :to="{name:'login'}">login</router-link>
-      </li>
-      <li>
-        <router-link :to="{name:'register'}">register</router-link>
-      </li>
-    </ul>
-  </header>
+  <dashboard v-if="userLogged" title="tet" :nav-links="navlinks">
+    <router-view></router-view>
+  </dashboard>
+  <empty v-if="!userLogged">
+    <header>
+      <ul class="nav">
+        <li>
+          <router-link :to="{name:'home'}">home</router-link>
+        </li>
+        <li>
+          <router-link :to="{name:'product.create'}">create product</router-link>
+        </li>
+        <li>
+          <router-link :to="{name:'login'}">login</router-link>
+        </li>
+        <li>
+          <router-link :to="{name:'register'}">register</router-link>
+        </li>
+      </ul>
+    </header>
+    <router-view></router-view>
+  </empty>
 
-  <router-view></router-view>
+
 </template>
 
 <style scoped>
@@ -38,11 +80,12 @@
   filter: drop-shadow(0 0 2em #42b883aa);
 }
 
-ul.nav{
+ul.nav {
   list-style: none;
   display: flex;
 }
-ul.nav a{
+
+ul.nav a {
   padding: 8px;
   display: block;
 }
